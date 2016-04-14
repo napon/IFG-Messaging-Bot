8415d9c279df31c7ab1f91644d2dec5ea199c097
Napon Taratan <napon@napontaratan.com>
2016-04-14 00:13:46 -0700

    Implemented query for the next upcoming IFG event.

--- ifgbot.js
+++ ifgbot.js
@@ -1,3 +1,8 @@
+if (!process.env.access_token) {
+    console.log('Error: Specify access_token in environment.');
+    process.exit(1);
+}
+
 if (!process.env.page_token) {
     console.log('Error: Specify page_token in environment');
     process.exit(1);
@@ -9,7 +14,11 @@ if (!process.env.verify_token) {
 }
 
 var Botkit = require('./lib/Botkit.js');
+var graph = require('fbgraph');
 var os = require('os');
+
+graph.setAccessToken(process.env.access_token);
+graph.setVersion('2.4');
 
 var controller = Botkit.facebookbot({
     debug: true,
@@ -26,209 +35,93 @@ controller.setupWebserver(process.env.port || 3000, function(err, webserver) {
     });
 });
 
-
-controller.hears(['hello', 'hi'], 'message_received', function(bot, message) {
+controller.hears(['hello', 'hi', 'hey'], 'message_received', function(bot, message) {
     controller.storage.users.get(message.user, function(err, user) {
         if (user && user.name) {
             bot.reply(message, 'Hello ' + user.name + '!!');
         } else {
-            bot.reply(message, 'Hello.');
+            bot.reply(message, "Hello! I'm a bot for IFG! You can ask me about our upcoming events or ask me for a Bible verse of the day! :)");
         }
     });
 });
 
+function getUpcomingEvents(cb) {
+    var events = [];
+    graph.get('/631063610257770/events', function(err, res) {
+        //console.log('res: ' + JSON.stringify(res.data));
+        if (err) {
+            console.log('err: ' + JSON.stringify(err));
+            cb([]);
+            return;
+        }
+        
+        var nextEvent = res.data[0];
+        var eventDate = new Date(nextEvent.start_time);
+        var todayDate = new Date();
+        if (eventDate.getTime() < todayDate) {
+             cb([]);
+             return;
+        }
 
-controller.hears(['structured'], 'message_received', function(bot, message) {
-
-    bot.reply(message, {
-        attachment: {
-            'type': 'template',
-            'payload': {
-                'template_type': 'generic',
-                'elements': [
+        graph.get('/' + nextEvent.id + '/picture?type=large', function(err2, picRes) {
+            console.log('err2: ' + JSON.stringify(err2));
+            console.log('picRes: ' + JSON.stringify(picRes));
+            var nextEventPost = {
+                'title': nextEvent.name,
+                'subtitle': nextEvent.place.name,
+                'image_url': picRes.location,
+                'buttons': [
                     {
-                        'title': 'Classic White T-Shirt',
-                        'image_url': 'http://petersapparel.parseapp.com/img/item100-thumb.png',
-                        'subtitle': 'Soft white cotton t-shirt is back in style',
-                        'buttons': [
-                            {
-                                'type': 'web_url',
-                                'url': 'https://petersapparel.parseapp.com/view_item?item_id=100',
-                                'title': 'View Item'
-                            },
-                            {
-                                'type': 'web_url',
-                                'url': 'https://petersapparel.parseapp.com/buy_item?item_id=100',
-                                'title': 'Buy Item'
-                            },
-                            {
-                                'type': 'postback',
-                                'title': 'Bookmark Item',
-                                'payload': 'White T-Shirt'
-                            }
-                        ]
-                    },
-                    {
-                        'title': 'Classic Grey T-Shirt',
-                        'image_url': 'http://petersapparel.parseapp.com/img/item101-thumb.png',
-                        'subtitle': 'Soft gray cotton t-shirt is back in style',
-                        'buttons': [
-                            {
-                                'type': 'web_url',
-                                'url': 'https://petersapparel.parseapp.com/view_item?item_id=101',
-                                'title': 'View Item'
-                            },
-                            {
-                                'type': 'web_url',
-                                'url': 'https://petersapparel.parseapp.com/buy_item?item_id=101',
-                                'title': 'Buy Item'
-                            },
-                            {
-                                'type': 'postback',
-                                'title': 'Bookmark Item',
-                                'payload': 'Grey T-Shirt'
-                            }
-                        ]
+                        'type': 'web_url',
+                        'url': 'https://www.facebook.com/events/' + nextEvent.id,
+                        'title': 'More info'
                     }
                 ]
-            }
-        }
-    });
-});
-
-controller.on('facebook_postback', function(bot, message) {
-    bot.reply(message, 'Great Choice!!!! (' + message.payload + ')');
-
-});
-
-controller.hears(['call me (.*)', 'my name is (.*)'], 'message_received', function(bot, message) {
-    var name = message.match[1];
-    controller.storage.users.get(message.user, function(err, user) {
-        if (!user) {
-            user = {
-                id: message.user,
             };
-        }
-        user.name = name;
-        controller.storage.users.save(user, function(err, id) {
-            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+
+            cb([nextEventPost]);
+            return;
         });
     });
-});
+}
 
-controller.hears(['what is my name', 'who am i'], 'message_received', function(bot, message) {
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Your name is ' + user.name);
+controller.hears(['the next event', 'coming up events', 'event coming up'], 'message_received', 
+    function(bot, message) {
+    getUpcomingEvents(function(events) {
+        if (events.length == 0) {
+            bot.reply(message, "Looks like we don't have an event coming up just yet! Stay tuned!");
         } else {
-            bot.startConversation(message, function(err, convo) {
-                if (!err) {
-                    convo.say('I do not know your name yet!');
-                    convo.ask('What should I call you?', function(response, convo) {
-                        convo.ask('You want me to call you `' + response.text + '`?', [
-                            {
-                                pattern: 'yes',
-                                callback: function(response, convo) {
-                                    // since no further messages are queued after this,
-                                    // the conversation will end naturally with status == 'completed'
-                                    convo.next();
-                                }
-                            },
-                            {
-                                pattern: 'no',
-                                callback: function(response, convo) {
-                                    // stop the conversation. this will cause it to end with status == 'stopped'
-                                    convo.stop();
-                                }
-                            },
-                            {
-                                default: true,
-                                callback: function(response, convo) {
-                                    convo.repeat();
-                                    convo.next();
-                                }
-                            }
-                        ]);
-
-                        convo.next();
-
-                    }, {'key': 'nickname'}); // store the results in a field called nickname
-
-                    convo.on('end', function(convo) {
-                        if (convo.status == 'completed') {
-                            bot.reply(message, 'OK! I will keep that in mind...');
-
-                            controller.storage.users.get(message.user, function(err, user) {
-                                if (!user) {
-                                    user = {
-                                        id: message.user,
-                                    };
-                                }
-                                user.name = convo.extractResponse('nickname');
-                                controller.storage.users.save(user, function(err, id) {
-                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-                                });
-                            });
-
-
-
-                        } else {
-                            // this happens if the conversation ended prematurely for some reason
-                            bot.reply(message, 'OK, nevermind!');
-                        }
-                    });
+            bot.reply(message, "Here is what's coming up!");
+            bot.reply(message, {
+                attachment: {
+                    'type': 'template',
+                    'payload': {
+                        'template_type': 'generic',
+                        'elements': events
+                    }
                 }
-            });
+            });    
         }
     });
 });
 
-//controller.hears(['shutdown'], 'message_received', function(bot, message) {
-
-    //bot.startConversation(message, function(err, convo) {
-
-        //convo.ask('Are you sure you want me to shutdown?', [
-            //{
-                //pattern: bot.utterances.yes,
-                //callback: function(response, convo) {
-                    //convo.say('Bye!');
-                    //convo.next();
-                    //setTimeout(function() {
-                        //process.exit();
-                    //}, 3000);
-                //}
-            //},
-        //{
-            //pattern: bot.utterances.no,
-            //default: true,
-            //callback: function(response, convo) {
-                //convo.say('*Phew!*');
-                //convo.next();
-            //}
-        //}
-        //]);
-    //});
-//});
-
-
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'], 'message_received',
+controller.hears(['identify yourself', 'who are you', 'your name', 'who made you'], 'message_received',
     function(bot, message) {
-
-        var hostname = os.hostname();
-        var uptime = formatUptime(process.uptime());
-
         bot.reply(message,
-            ':robot_face: I am a bot named <@' + bot.identity.name +
-             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-    });
-
-
-
-controller.on('message_received', function(bot, message) {
-    bot.reply(message, 'Try: `what is my name` or `structured` or `call me captain`');
-    return false;
+            "Beep! Boop! I am a bot created by IFG. You can report your concerns directly to my master at <ifg.ivcf@gmail.com>");
 });
 
+controller.hears(['uptime', 'how old are you'], 'message_received',
+    function(bot, message) {
+        var uptime = formatUptime(process.uptime());
+        bot.reply(message,
+            "I have been running for " + uptime + ".");
+});
+
+controller.on('message_received', function(bot, message) {
+    bot.reply(message, "I'm not that smart yet! Try asking me for the next event or for a bible verse!");
+    return false;
+});
 
 function formatUptime(uptime) {
     var unit = 'second';
